3fce619b08a55e243a1a15ccc283d90709ef83c7
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-02-23 22:01:45 -0500

    Settings are now working

---
+++ scripts/apply_settings.js
@@ -0,0 +1,46 @@
+const Files = {
+    "css/subscribe_button.css":"subscribe",
+    "css/join_button.css":"join",
+    "css/like_button.css":"like",
+    "css/uppercase_letters.css":"uppercase",
+    "css/rectangle_buttons.css":"rectangle"
+}
+
+// runs on setting change
+chrome.storage.onChanged.addListener((changes,namespace) => {
+    for(let key in changes) {
+        if(key === "settings") {
+            const newSettings = changes[key].newValue;
+            UpdateFiles(newSettings);
+        }
+    }
+});
+// runs on page load
+chrome.storage.local.get("settings",function(result) {
+    const settings = result.settings || {};
+    UpdateFiles(settings);
+})
+
+function UpdateFiles(settings) {
+    for(let file in Files) {
+        const optionKey = Files[file];
+        const isEnabled = settings[optionKey];
+        console.log(`File: ${file}, Option key: ${optionKey}, Is enabled: ${isEnabled}`);
+        if(isEnabled) {
+            AddFile(file);
+        } else {
+            RemoveFile(file);
+        }
+    }
+}
+function AddFile(file) {
+    const link = document.createElement("link");
+    link.href = chrome.runtime.getURL(file);
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+}
+function RemoveFile(file) {
+    const links = document.querySelectorAll(`link[href="${chrome.runtime.getURL(file)}"]`);
+    links.forEach(link => link.remove());
+}
